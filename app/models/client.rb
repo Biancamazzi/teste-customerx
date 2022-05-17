1f9a323a098d477467f9fd2adfb87c8510f033ba
BiancaMazzi <bianca.mazzi@hotmail.com>
2022-05-16 21:14:56 -0300

    small updates

--- app/models/client.rb
+++ app/models/client.rb
@@ -1,7 +1,7 @@
 class Client < ApplicationRecord
-    has_many :contacts, dependent: :destroy
-    validates :email,
-        format: { with: /(.+)@(.+)/, message: "Email invalid"  },
-            uniqueness: { case_sensitive: false },
-            length: { minimum: 4, maximum: 254 }  
+  has_many :contacts, dependent: :destroy
+  validates :email,
+      format: { with: /(.+)@(.+)/, message: "Email invalid"  },
+          uniqueness: { case_sensitive: false },
+          length: { minimum: 4, maximum: 254 }
 end
--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,7 +1,7 @@
 class Contact < ApplicationRecord
   belongs_to :client
   validates :email,
-  format: { with: /(.+)@(.+)/, message: "Email invalid"  },
-            uniqueness: { case_sensitive: false },
-            length: { minimum: 4, maximum: 254 }  
+      format: { with: /(.+)@(.+)/, message: "Email invalid"  },
+          uniqueness: { case_sensitive: false },
+          length: { minimum: 4, maximum: 254 }
 end
